dd879a9c5bab4e70e40401ce1fc9b449eb37c96a
jorgerdz <jorgerdz73@gmail.com>
2022-04-05 07:35:00 -0500

    Estado inicial del proyecto

---
+++ afirmetest/src/main/java/com/example/afirmetest/AfirmetestApplication.java
@@ -0,0 +1,13 @@
+package com.example.afirmetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfirmetestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfirmetestApplication.class, args);
+	}
+
+}
